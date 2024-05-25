5fae2a3d2d543ed2a79defea6640fd9a4f734571
KenenthAtchon <kenneth.atchon@hotmail.com>
2024-05-24 19:46:01 -0500

    Start!

---
+++ my-react-app/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './index.css';
+
+function App() {
+  return (
+    <div className="bg-gray-200 p-4">
+      <h1 className="text-2xl font-bold text-blue-600">Hello, Tailwind CSS!</h1>
+      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4">
+        Click Me
+      </button>
+    </div>
+  );
+}
+
+export default App;
